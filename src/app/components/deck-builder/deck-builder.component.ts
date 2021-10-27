f62084b72cc05012140a2d877d323a2ac993e7ea
PedroEdu6786 <pedrcg835@gmail.com>
2021-10-27 18:28:44 -0500

    feat: add dynamic list of cards to deck builder

--- src/app/components/deck-builder/deck-builder.component.ts
+++ src/app/components/deck-builder/deck-builder.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Cards } from 'src/app/interface/yugioh/cards';
 import { DeckBuilderService } from 'src/app/service/builder/deck-builder.service';
-import { DeckType } from 'src/app/service/utils/deckTypes';
+import { Deck, DeckType } from 'src/app/service/utils/deckTypes';
+import { deckFactory } from 'src/app/service/utils/factories/deckFactory';
 
 @Component({
   selector: 'app-deck-builder',
@@ -11,7 +11,9 @@ import { DeckType } from 'src/app/service/utils/deckTypes';
 })
 export class DeckBuilderComponent implements OnInit {
   public cardType!: string;
-  public cardsData!: Cards;
+  public cardsData!: Deck;
+  public isLoading = false;
+  public currPage = 1;
 
   constructor(
     private builderService: DeckBuilderService,
@@ -21,16 +23,24 @@ export class DeckBuilderComponent implements OnInit {
   async ngOnInit() {
     let cardType = this.route.snapshot.paramMap.get('cardType')!;
     cardType = cardType.charAt(0).toUpperCase() + cardType.slice(1);
-    this.cardType = cardType
+    this.cardType = cardType;
 
-    await this.builderService.getAllCards(1, DeckType[cardType as DeckType]).then(
-      async (response) => {
-        this.cardsData = response;
-      },
-      (error) => {
-        alert('error' + error.statusNext);
-      }
-    );
+    this.isLoading = true;
+    await this.builderService
+      .getAllCards(this.currPage, DeckType[cardType as DeckType])
+      .then(
+        async (response) => {
+          this.cardsData = deckFactory(
+            response,
+            DeckType[cardType as DeckType]
+          );
+        },
+        (error) => {
+          alert('error' + error.statusNext);
+        }
+      );
+
+    this.isLoading = false;
 
     console.log(this.cardsData);
   }
--- src/app/service/utils/deckTypes.ts
+++ src/app/service/utils/deckTypes.ts
@@ -4,12 +4,19 @@ export enum DeckType {
   Magic = 'Magic',
 }
 
+interface Card {
+  img: string;
+  desc: string;
+  name: string;
+}
+export interface Deck {
+  data: Array<Card>;
+}
+
 const pageSize = 10;
-//page=10
+
 const POKEMON_API = `https://api.pokemontcg.io/v2/cards?pageSize=${pageSize}`;
 const MAGIC_API = `https://api.magicthegathering.io/v1/cards?pageSize=${pageSize}`;
-
-//offset=(page-1)*16
 const YUGIOH_API = `https://db.ygoprodeck.com/api/v7/cardinfo.php?num=${pageSize}`;
 
 export const deckURI = {
